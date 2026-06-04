b39e9e78c83397b41b97381afdce378ef177931b
Sachin Kumar Mishra <165578484+SachinKrMishra@users.noreply.github.com>
2026-06-05 01:25:30 +0530

    Logging and Exception Handling Completed

---
+++ src/logging/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+# Create logs directory
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Create log file name
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+
+# Complete log file path
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
